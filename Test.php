c4e93db9e9e54673f40e963f20a81e3c5184b879
babausaemail <babausaemail@gmail.com>
2014-10-13 18:27:13 -0700

    add README.md

---
+++ Test.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Test Header Cache</title>
+<?php 
+  //header('Cache-Control: max-age=3600');
+  header('Cache-Control: no-cache');
+  header('Pragma-directive: no-cache');
+  header('Cache-directive: no-cache');
+  header('Pragma: no-cache');
+  header('Expires: 0');
+  //echo "JI";
+?>
+</head>
+
+<body>
+
+
+<h1> Hello </h1>
+<img src="../image/data/product/COACH/Accessories   CLEARANCE   Coach Factory Official Site.png" />
+<form method="get" action="Test_display.php">
+	Name: <input type="text" name="name_key" />
+    <input type="submit" value="Submit_key" />
+</form>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+<p> adfjadlfjadlskfjasldfjadfjlsdfkjalsdkf </p>
+</body>
+</html>
